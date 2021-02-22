b18accfe67cc7fa9bac851940b72344b364fbabc
Piscis Xalock <jabadg99@gmail.com>
2021-02-22 13:25:49 +0100

    Versión 1 ~Ejercicio 8.1 CRUD Productos~ Borrar.

---
+++ src/main/java/Modelo/ProductosCRUD.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import javax.persistence.*;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ProductosCRUD {
+
+    public static int destroyProducto(int id) {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("com.mycompany_8.1_CRUD_Productos_war_1.0-SNAPSHOTPU");
+        EntityManager manager = factory.createEntityManager();
+        String sql = "DELETE from Productos p WHERE p.id = " + id;
+        Query q = manager.createQuery(sql);
+        manager.getTransaction().begin();
+        int filasAfectadas = q.executeUpdate(); //para las consultas de modif. datos se usa el método executeUpdate
+        manager.getTransaction().commit();
+        return filasAfectadas;
+    }
+
+}
